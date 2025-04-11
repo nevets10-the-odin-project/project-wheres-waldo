68fd47abb276a846d3d65b20fbfe75362cbd690f
Nevets10 <12600557+Nevets10@users.noreply.github.com>
2025-04-11 11:00:13 -0700

    Correct Game creation logic

--- app/controllers/api/games_controller.rb
+++ app/controllers/api/games_controller.rb
@@ -1,8 +1,6 @@
 class Api::GamesController < ApplicationController
   def create
-    p session[:image_id]
-    @game = Image.find(session[:image_id].to_i).games.create
-    @game[:start_time] = Time.now
+    @game = Image.find(session[:image_id].to_i).games.create(start_time: Time.now, session_id: session[:image_id].to_i)
 
     if @game.save
       session[:game_id] = @game[:id]
