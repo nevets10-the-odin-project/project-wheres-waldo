509a1200bced62684e86fe8b1211720ab110350b
Nevets10 <12600557+Nevets10@users.noreply.github.com>
2025-04-11 11:16:07 -0700

    Add guard clause for existing game session and img

--- app/controllers/api/games_controller.rb
+++ app/controllers/api/games_controller.rb
@@ -1,6 +1,10 @@
 class Api::GamesController < ApplicationController
   def create
-    @game = Image.find(session[:image_id].to_i).games.create(start_time: Time.now, session_id: session[:image_id].to_i)
+    @existing_game = Game.find_by(session_id: request.session_options[:id].to_s, image_id: session[:image_id])
+    return head :ok if @existing_game
+
+    start_params = { start_time: Time.now, session_id: request.session_options[:id] }
+    @game = Image.find(session[:image_id].to_i).games.create(start_params)
 
     if @game.save
       session[:game_id] = @game[:id]
