92f8f96185415a0280f957ac394eaf0f19af9f5a
Nevets10 <12600557+Nevets10@users.noreply.github.com>
2025-04-16 16:59:51 -0700

    Pull image games into state for leaderboard

--- app/javascript/components/leaderboard/Leaderboard.jsx
+++ app/javascript/components/leaderboard/Leaderboard.jsx
@@ -3,7 +3,17 @@ import Nav from "../nav/Nav";
 import styles from "./leaderboard.module.css";
 
 export default function Leaderboard() {
-	const [imgData, useImgData] = useState();
+	const [imgData, setImgData] = useState(undefined);
+
+	useEffect(() => {
+		try {
+			fetch(`http://127.0.0.1:3000/api/leaderboard/`)
+				.then((res) => res.json())
+				.then((data) => setImgData(data));
+		} catch (error) {
+			console.log(error);
+		}
+	}, []);
 
 	return (
 		<>
