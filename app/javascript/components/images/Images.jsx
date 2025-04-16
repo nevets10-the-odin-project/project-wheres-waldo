8a42ea3b4294de6b061945605937433138574381
Nevets10 <12600557+Nevets10@users.noreply.github.com>
2025-04-15 17:32:34 -0700

    Add image state, map image titles in images comp

--- app/javascript/components/images/Images.jsx
+++ app/javascript/components/images/Images.jsx
@@ -1,15 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Nav from "../nav/Nav";
 import Image from "../image/Image";
 import { useParams } from "react-router-dom";
 
 export default function Images() {
+	const [images, setImages] = useState([]);
 	const { id } = useParams();
+
+	useEffect(() => {
+		try {
+			fetch(`http://127.0.0.1:3000/api/images/`)
+				.then((res) => res.json())
+				.then((data) => setImages(data));
+		} catch (error) {
+			console.log(error);
+		}
+	}, []);
 
 	return (
 		<>
 			<Nav />
-			{id === undefined ? <div>Images Index</div> : <Image />}
+			{id === undefined ? (
+				images.map((image) => <div>{image.name}</div>)
+			) : (
+				<Image />
+			)}
 		</>
 	);
 }
