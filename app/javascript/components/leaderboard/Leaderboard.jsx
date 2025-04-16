935d0ab39fc214c2d061fc7b33879faaf78cf3ed
Nevets10 <12600557+Nevets10@users.noreply.github.com>
2025-04-16 16:54:26 -0700

    Add imgData state to Leaderboard

--- app/javascript/components/leaderboard/Leaderboard.jsx
+++ app/javascript/components/leaderboard/Leaderboard.jsx
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Nav from "../nav/Nav";
 import styles from "./leaderboard.module.css";
 
 export default function Leaderboard() {
+	const [imgData, useImgData] = useState();
+
 	return (
 		<>
 			<Nav />
-			<div>Leaderboard</div>
+			<div></div>
 		</>
 	);
 }
