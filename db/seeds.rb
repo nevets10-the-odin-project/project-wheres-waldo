3cc2ceeb431386f143ecc8f71ab676c2e5193cf9
Nevets10 <12600557+Nevets10@users.noreply.github.com>
2025-04-16 19:02:12 -0700

    Add characters to other two images

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,13 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
-
 %w[Waldo Wilma Wizard Odlaw Woof].each do |name|
   Character.find_or_create_by!(name: name)
 end
 
 [
-  { name: 'In Town', file_name: 'https://ia800402.us.archive.org/1/items/wheres-waldo-archive/01%20-%20Where%27s%20Waldo/1997%20-%20Where%27s%20Waldo%20Special%20Edition/book1-1997-scene01-in-town-600dpi.jpg' }
+  { name: 'In Town',
+    file_name: 'https://ia800402.us.archive.org/1/items/wheres-waldo-archive/01%20-%20Where%27s%20Waldo/1997%20-%20Where%27s%20Waldo%20Special%20Edition/book1-1997-scene01-in-town-600dpi.jpg' },
+  { name: 'On The Beach',
+    file_name: 'https://ia800402.us.archive.org/1/items/wheres-waldo-archive/01%20-%20Where%27s%20Waldo/1997%20-%20Where%27s%20Waldo%20Special%20Edition/book1-1997-scene02-on-the-beach-600dpi.jpg' },
+  { name: 'Fairground', file_name: 'https://ia800402.us.archive.org/1/items/wheres-waldo-archive/01%20-%20Where%27s%20Waldo/1997%20-%20Where%27s%20Waldo%20Special%20Edition/book1-1997-scene12-fairground-600dpi.jpg' }
 ].each do |image|
   Image.find_or_create_by!(name: image[:name], file_name: image[:file_name])
 end
@@ -58,6 +52,86 @@ end
     start_y: 390,
     end_x: 1187,
     end_y: 407
+  },
+  {
+    image_id: 2,
+    character_id: 1,
+    start_x: 1249,
+    start_y: 441,
+    end_x: 1290,
+    end_y: 523
+  },
+  {
+    image_id: 2,
+    character_id: 2,
+    start_x: 1582,
+    start_y: 491,
+    end_x: 1608,
+    end_y: 524
+  },
+  {
+    image_id: 2,
+    character_id: 3,
+    start_x: 528,
+    start_y: 415,
+    end_x: 568,
+    end_y: 464
+  },
+  {
+    image_id: 2,
+    character_id: 4,
+    start_x: 181,
+    start_y: 420,
+    end_x: 205,
+    end_y: 484
+  },
+  {
+    image_id: 2,
+    character_id: 5,
+    start_x: 1392,
+    start_y: 453,
+    end_x: 1409,
+    end_y: 465
+  },
+  {
+    image_id: 3,
+    character_id: 1,
+    start_x: 1490,
+    start_y: 651,
+    end_x: 1517,
+    end_y: 699
+  },
+  {
+    image_id: 3,
+    character_id: 2,
+    start_x: 193,
+    start_y: 974,
+    end_x: 219,
+    end_y: 1019
+  },
+  {
+    image_id: 3,
+    character_id: 3,
+    start_x: 839,
+    start_y: 726,
+    end_x: 879,
+    end_y: 774
+  },
+  {
+    image_id: 3,
+    character_id: 4,
+    start_x: 1195,
+    start_y: 343,
+    end_x: 1218,
+    end_y: 374
+  },
+  {
+    image_id: 3,
+    character_id: 5,
+    start_x: 939,
+    start_y: 1123,
+    end_x: 965,
+    end_y: 1141
   }
 ].each do |coord|
   Coordinate.find_or_create_by!(
