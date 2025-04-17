5806c5b15f94d8a2d3a661a544cb969cdc5c0763
Nevets10 <12600557+Nevets10@users.noreply.github.com>
2025-04-16 19:57:45 -0700

    Update homepage text

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -15,21 +15,24 @@ function App() {
 					</a>
 					. It is the first of two projects to really test the student's full-stack
 					knowledge. In my case, I have been going through the Ruby on Rails path, so
-					that is what makes up the back-end of this project.
-				</p>
-				<p>
-					This project basically implements the game{" "}
+					the back-end uses Ruby on Rails.
+					<br />
+					<br />
+					This project basically implements the children's puzzle book{" "}
 					<a href="https://en.wikipedia.org/wiki/Where's_Wally">Where's Waldo</a>.
 					Players will look at a busy image filled with dozens and dozens of
-					characters doing various tasks. The goal is to find Waldo (or other
-					specified characters) as quickly as possible. A leaderboard will show the
+					characters doing various things. The goal is to find Waldo (or other
+					specified characters) as quickly as possible. The leaderboard shows the
 					players with the fastest times.
 				</p>
 				<h2>Pro Tip</h2>
 				<p>
-					The images are very large. To zoom out, press <strong>CTRL</strong> &{" "}
-					<strong>- key</strong>. To zoom in, press <strong>CTRL</strong> &{" "}
-					<strong>+ key</strong>
+					To zoom out, press <strong>CTRL</strong> & <strong>- key</strong>. To zoom
+					in, press <strong>CTRL</strong> & <strong>+ key</strong>
+				</p>
+				<p>
+					The images are very large. I don't want to host them myself for copyright
+					reasons, so I cannot edit them.
 				</p>
 			</div>
 		</>
