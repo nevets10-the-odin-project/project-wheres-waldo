eb2e7af733d12ea58f16fc520f64a458608a8412
Nevets10 <12600557+Nevets10@users.noreply.github.com>
2025-04-17 16:20:58 -0700

    Implement loading icon

--- app/javascript/components/images/Images.jsx
+++ app/javascript/components/images/Images.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import Icon from "@mdi/react";
+import { mdiLoading } from "@mdi/js";
 import styles from "./images.module.css";
 import Nav from "../nav/Nav";
 import ImagePage from "../imagePage/ImagePage";
@@ -7,13 +9,17 @@ import ImageCard from "../imageCard/ImageCard";
 
 export default function Images() {
 	const [images, setImages] = useState([]);
+	const [isLoading, setIsLoading] = useState(true);
 	const { id } = useParams();
 
 	useEffect(() => {
 		try {
 			fetch(`/api/images/`)
 				.then((res) => res.json())
-				.then((data) => setImages(data));
+				.then((data) => {
+					setImages(data);
+					setIsLoading(false);
+				});
 		} catch (error) {
 			console.log(error);
 		}
@@ -23,7 +29,9 @@ export default function Images() {
 		<>
 			<Nav />
 			<div className={styles.imagesWrapper}>
-				{id === undefined ? (
+				{isLoading ? (
+					<Icon path={mdiLoading} spin={1} size={10} />
+				) : id === undefined ? (
 					images.map((image) => <ImageCard key={image.id} image={image} />)
 				) : (
 					<ImagePage />
