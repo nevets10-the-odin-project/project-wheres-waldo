93014431f26c0ce40342bd5c86d84e71417ff2f1
Nevets10 <12600557+Nevets10@users.noreply.github.com>
2025-04-16 17:18:38 -0700

    Populate ImageLeaderboard, sort games by game time

--- app/javascript/components/imageLeaderboard/ImageLeaderboard.jsx
+++ app/javascript/components/imageLeaderboard/ImageLeaderboard.jsx
@@ -1,6 +1,22 @@
 import React from "react";
 import styles from "./imageLeaderboard.module.css";
 
+function compareGames(g1, g2) {
+	const diff1 = new Date(g1.end_time) - new Date(g1.start_time);
+	const diff2 = new Date(g2.end_time) - new Date(g2.start_time);
+	return diff1 - diff2;
+}
+
 export default function ImageLeaderboard({ image }) {
-	return <div>{image.name}</div>;
+	const filteredGame = image.games.filter((game) => game.end_time != null);
+	const sortedGames = filteredGame.toSorted(compareGames);
+
+	return (
+		<div>
+			<h2>{image.name}</h2>
+			{sortedGames?.map((game) => (
+				<div>{game.initials}</div>
+			))}
+		</div>
+	);
 }
