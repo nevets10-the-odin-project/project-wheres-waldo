2ad1b18983fef0278245f91b5a4fbc42fde05e5a
Nevets10 <12600557+Nevets10@users.noreply.github.com>
2025-04-17 17:27:40 -0700

    Add loading indicator to initials form submit

--- app/javascript/components/finishBanner/FinishBanner.jsx
+++ app/javascript/components/finishBanner/FinishBanner.jsx
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import Icon from "@mdi/react";
+import { mdiLoading } from "@mdi/js";
 import styles from "./finishBanner.module.css";
 
 export default function FinishBanner({ gameData, handleInitialsSubmit }) {
+	const [isLoading, setIsLoading] = useState(false);
+
 	const diff = new Date(gameData.end_time) - new Date(gameData.start_time);
 
 	const minutes = Math.floor(diff / (1000 * 60));
 	const seconds = Math.floor(diff / 1000) - minutes * 60;
+
+	function submitInitials(e) {
+		setIsLoading(true);
+		handleInitialsSubmit(e);
+	}
 
 	return (
 		<div className={styles.banner}>
@@ -14,8 +23,10 @@ export default function FinishBanner({ gameData, handleInitialsSubmit }) {
 				<p>
 					{gameData.initials} - Time: {minutes} Minute(s), {seconds} Second(s)
 				</p>
+			) : isLoading ? (
+				<Icon path={mdiLoading} spin={1} size={10} />
 			) : (
-				<form onSubmit={handleInitialsSubmit} className={styles.form}>
+				<form onSubmit={submitInitials} className={styles.form}>
 					<label>
 						Initials:
 						<input name="initials" maxLength="3" />
