62c669e43417973f88b17098ef08de6a0885553f
Nevets10 <12600557+Nevets10@users.noreply.github.com>
2025-04-17 18:05:04 -0700

    Add show state for mobile menu

--- app/javascript/components/nav/Nav.jsx
+++ app/javascript/components/nav/Nav.jsx
@@ -1,17 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import Icon from "@mdi/react";
 import { mdiMenu } from "@mdi/js";
 import styles from "./nav.module.css";
 import { Link } from "react-router-dom";
 
 export default function Nav() {
+	const [showNav, setShowNav] = useState(true);
+
+	function toggleMenu() {
+		setShowNav(!showNav);
+	}
 	return (
 		<>
 			<div className={styles.nav}>
-				<Icon className={styles.menuIcon} path={mdiMenu} size={2} color="white" />
-				<Link to="/">Home</Link>
-				<Link to="/images">Images</Link>
-				<Link to="/leaderboard">Leaderboard</Link>
+				<div className={styles.iconWrapper} onClick={toggleMenu}>
+					<Icon className={styles.menuIcon} path={mdiMenu} size={2} color="white" />
+				</div>
+				{showNav && (
+					<>
+						<Link to="/">Home</Link>
+						<Link to="/images">Images</Link>
+						<Link to="/leaderboard">Leaderboard</Link>
+					</>
+				)}
 			</div>
 		</>
 	);
