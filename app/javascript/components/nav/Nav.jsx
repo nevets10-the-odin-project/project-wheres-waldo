93640e66e96e8844b7821002ac4e0d6e2fc07c5f
Nevets10 <12600557+Nevets10@users.noreply.github.com>
2025-04-17 17:57:24 -0700

    Adjust Nav and imagePage styles

--- app/javascript/components/nav/Nav.jsx
+++ app/javascript/components/nav/Nav.jsx
@@ -1,13 +1,18 @@
 import React from "react";
+import Icon from "@mdi/react";
+import { mdiMenu } from "@mdi/js";
 import styles from "./nav.module.css";
 import { Link } from "react-router-dom";
 
 export default function Nav() {
 	return (
-		<div className={styles.nav}>
-			<Link to="/">Home</Link>
-			<Link to="/images">Images</Link>
-			<Link to="/leaderboard">Leaderboard</Link>
-		</div>
+		<>
+			<div className={styles.nav}>
+				<Icon className={styles.menuIcon} path={mdiMenu} size={2} color="white" />
+				<Link to="/">Home</Link>
+				<Link to="/images">Images</Link>
+				<Link to="/leaderboard">Leaderboard</Link>
+			</div>
+		</>
 	);
 }
